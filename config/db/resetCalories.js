8ac5e03cc4fb122393de90975a82a3e1b7998ff3
Roy Chowdhury <chowdhury.roy4@gmail.com>
2018-08-03 15:54:24 -0600

    Added authorization routes and added db checking every 24 hours

--- config/db/resetCalories.js
+++ config/db/resetCalories.js
@@ -1,10 +1,7 @@
 const cron = require("node-schedule");
-const rule = new cron.RecurrenceRule();
 
 const db = require("./database");
 
-rule.hour = 24;
-rule.minute = 0;
-cron.scheduleJob(rule, function() {
+cron.scheduleJob("0 0 * * *", function() {
   db("users").update("daily_calorie", 0);
 });

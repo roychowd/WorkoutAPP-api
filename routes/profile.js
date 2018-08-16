1295462c875dfcee27a5c59cacf255ee85ab2389
Roy Chowdhury <chowdhury.roy4@gmail.com>
2018-08-16 13:03:43 -0600

    added daily cal updater. need to check to see if it is a number

--- routes/profile.js
+++ routes/profile.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../config/db/userModel");
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
+
 
 router.get("/", (req, res) => {
   res.send("hi");
 });
 
-router.put("/:id/overall_calories", (req, res) => {
-  const id = req.params.id;
+router.put("/:id/dailyCal", (req, res) => {
   const desiredCalories = req.body.calories;
-  db("usersTable")
-    .where("id", "=", id)
-    .update({
-      daily_Calories: desiredCalories
+  User.forge({ id: req.params.id })
+    .fetch({ require: true })
+    .then(user => {
+      user
+        .save({
+          daily_Calories: desiredCalories
+        })
+        .catch(err => {
+          res.status(500).json({ error: true, data: err });
+        });
     })
-    .returning(["daily_Calories"])
-    .then(calories => res.json(calories))
-    .catch(err => res.status(400).json("error putting users Caloric Goals"));
+    .catch(err => res.status(500).json({ error: true, data: err }));
 });
 
 module.exports = router;

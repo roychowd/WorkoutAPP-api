f521ee55bef633782f9bbbca382dba235a99ae3a
Roy Chowdhury <chowdhury.roy4@gmail.com>
2018-08-29 21:59:48 -0600

    added some stuff to exercise routes

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -7,7 +7,13 @@ router.get("/", (req, res) => {
 });
 
 router.get("/category/:muscle", (req, res) => {
-  console.log(req.params.muscle);
+  // console.log(req.params.muscle);
+  if (req.params.muscle === "upperArm") {
+    var muscle = "Upper Arm";
+  } else {
+    var muscle = req.params.muscle;
+  }
+  console.log(muscle);
   //   exercises.forge({ muscle: req.params.muscle }).fetchAll();
 });
 
